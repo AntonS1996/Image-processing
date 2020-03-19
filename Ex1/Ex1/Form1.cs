4d71f1498a82da82c0de8767ea67d597cdfaca28
Anton Skvortsov <anton.skvortsov.1996@yandex.ru>
2020-03-19 22:30:21 +0300

    Upload changes

--- Ex1/Ex1/Form1.cs
+++ Ex1/Ex1/Form1.cs
@@ -17,7 +17,6 @@ namespace Ex1
             InitializeComponent();
         }
 
-        // Bitmap bmp = new Bitmap("C:\\Users\\Anton\\source\\repos\\K-means\\Izmaylovo_2014.bmp");
         Bitmap bmp = null;
         byte i1, i2, i3;
         Image MemForImage;
@@ -26,7 +25,6 @@ namespace Ex1
 
         private void LoadImage(bool Bmp)
         {
-            // openFileDialog1.InitialDirectory = "C:\\Users\\Anton\\source\\repos\\K-means";
             openFileDialog1.InitialDirectory = "";
 
             if (Bmp)
